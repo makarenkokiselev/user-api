e7524ae0dd68dc7dee0ace906994afadd5a39213
makarenko kiselev <prrjavadeveloper@gmail.com>
2019-05-17 20:37:55 +0200

    user controller modified

--- src/main/java/com/gaussoft/spike/controller/UserController.java
+++ src/main/java/com/gaussoft/spike/controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-    @GetMapping("/hello")
+    @GetMapping(value = "/hello")
     public ResponseEntity<String> helloWorld() {
         return ResponseEntity.ok("Hello World!");
     }
