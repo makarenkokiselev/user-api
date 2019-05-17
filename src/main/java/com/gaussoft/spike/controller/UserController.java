fb9ae58d1ad659c219d948b5b837906602dc180d
makarenko kiselev <prrjavadeveloper@gmail.com>
2019-05-17 20:00:51 +0200

    user controller added

---
+++ src/main/java/com/gaussoft/spike/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.gaussoft.spike.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> helloWorld() {
+        return ResponseEntity.ok("Hello World!");
+    }
+
+}
